b57da3ef819b17408a0f1f5d63090b6d1560f107
dancelifeee <leralikhomanova@gmail.com>
2025-03-31 20:40:25 +0200

    huinya

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,14 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    {/* 👇 Оборачиваем App в BrowserRouter и указываем basename */}
-    <BrowserRouter basename="/readtimer">
+    {/* 👇 Оборачиваем App в BrowserRouter без basename */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
