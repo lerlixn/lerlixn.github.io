9c180719ee4f6c6471642d100e02af64817f5569
dancelifeee <leralikhomanova@gmail.com>
2025-03-31 20:46:22 +0200

    huinya 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TimerPage from "./pages/TimerPage";
 import BooksPage from "./pages/BooksPage";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/timer" element={<TimerPage />} />
-        <Route path="/books" element={<BooksPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/timer" element={<TimerPage />} />
+      <Route path="/books" element={<BooksPage />} />
+    </Routes>
   );
 };
 
