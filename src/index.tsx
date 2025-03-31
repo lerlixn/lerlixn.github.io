659aa7b0dc2bea22d78591a0cfc01503f38deaad
dancelifeee <leralikhomanova@gmail.com>
2025-03-31 20:07:29 +0200

    github pages

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// 👇 Импортируем BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    {/* 👇 Оборачиваем App в BrowserRouter и указываем basename */}
+    <BrowserRouter basename="/readtimer">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
